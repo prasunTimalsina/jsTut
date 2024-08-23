5dd3e3f6f39cb25a0982dd1b9046fe6e5c9bfc92
prasunTimalsina <168793057+prasunTimalsina@users.noreply.github.com>
2024-08-23 07:54:46 +0000

    day1Complete

---
+++ 01_Day/dataTypeSummary.js
@@ -0,0 +1,6 @@
+const id = Symbol('111');
+const anotherId = Symbol('111');
+
+console.log(id === anotherId);
+const bigNumber = 321313131313131313654654n;
+console.log(typeof bigNumber);
--- 01_Day/test.js
+++ 01_Day/test.js
@@ -2,5 +2,6 @@ const userId = 321321
 let userEmail = "exmaple@gmail.com";
 var userPassword  = "Random@#Generated";
 userCity = "Banepa";
+let userState;
 
-console.table([userId,userEmail,userPassword,userCity]);
+console.table([userId,userEmail,userPassword,userCity,userState]);
