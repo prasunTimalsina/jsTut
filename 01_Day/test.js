dcaf6bfa01277c63073e0e5ea134b96a49037c35
prasunTimalsina <168793057+prasunTimalsina@users.noreply.github.com>
2024-08-23 06:45:48 +0000

    newconfig

---
+++ 01_Day/test.js
@@ -0,0 +1,6 @@
+const userId = 321321
+let userEmail = "exmaple@gmail.com";
+var userPassword  = "Random@#Generated";
+userCity = "Banepa";
+
+console.table([userId,userEmail,userPassword,userCity]);
